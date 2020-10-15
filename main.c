fb7a8121d4aec1193d4f5c341b36f9f4b07a8aa8
John Doe <jdoe35087@gmail.com>
2020-10-14 23:33:46 -0400

    add test-runner output to file

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 #include <module.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stddef.h>
 
 int main(void)
 {
   int c = addition(10, 1);
+  FILE* file = fopen("test-output", "w+c");
+  if (file == NULL) {
+	strerror(errno);
+	return -1;
+  }
+  
+  if (c != 11) {
+	fputs("test failed\n", file);
+	fclose(file);
+	return -1;
+  }
 
-  if (c != 10)
-	{
-	  fputs("test failed", stderr);
-	  return -1;
-	}
-
-  else
+  else {
+	fputs("test ok\n", file);
+	fclose(file);
 	return 0;
+  }
+
+  
 }
