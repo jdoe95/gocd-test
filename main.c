e840566892e68fb9f900a171ce9d512a875040cc
John Doe <jdoe35087@gmail.com>
2020-10-14 23:11:04 -0400

    add stderr output

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <module.h>
+#include <stdio.h>
 
 int main(void)
 {
   int c = addition(10, 1);
 
   if (c != 11)
-	return -1;
+	{
+	  fputs("test failed", stderr);
+	  return -1;
+	}
 
   else
 	return 0;
